9b9d3a0724e425343bd49d6660130a53a93d65de
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-13 20:09:41 +0530

    Add files via upload

---
+++ Oderby.java
@@ -0,0 +1,17 @@
+
+//Order by
+import java.util.*;
+
+public class Oderby {
+
+    public static void main(String args[]) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(5);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+    }
+}
+
+// output:- 1, 2, 3, 5
