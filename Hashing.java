e982027e7a2c119c72fbaf58bbac7d4ace8f6a43
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-13 19:24:52 +0530

    Add files via upload

--- Hashing.java
+++ Hashing.java
@@ -1,27 +1,21 @@
 
-//Sliding window Maximum
+//Majority Element in The Program
 import java.util.*;
 
 public class Hashing {
 
     public static void main(String args[]) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        // Insert -O(1)
-        hm.put("India", 100);
-        hm.put("China", 150);
-        hm.put("Us", 50);
-        System.out.println(hm);
-
-        // Size:-
-        System.out.println(hm.size());
-        // It Empty
-        hm.clear();
-        System.out.println(hm.isEmpty());
+        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            for (Integer Key : map.keySet()) {
+                if (map.get(Key) > arr.length / 3) {
+                    System.out.println(Key);
+                }
+            }
+        }
     }
 }
 
-/*
- * Output:-{China=150, Us=50, India=100}
- * 3
- * true
- */
+// output:- 1
